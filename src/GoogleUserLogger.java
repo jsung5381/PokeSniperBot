f50d32b74c1c2a6cbe8d34cf7bd8c18fe27d037d
jsung5381 <jsung5381@naver.com>
2017-02-10 22:03:25 +0900

    Refactored GoogleUserLogger class so that it doesn't have to copy and paste the auth code.

--- src/GoogleUserLogger.java
+++ src/GoogleUserLogger.java
@@ -1,7 +1,10 @@
+import com.pokegoapi.api.PokemonGo;
+import com.pokegoapi.auth.GoogleAutoCredentialProvider;
 import com.pokegoapi.auth.GoogleUserCredentialProvider;
 import com.pokegoapi.exceptions.CaptchaActiveException;
 import com.pokegoapi.exceptions.LoginFailedException;
 import com.pokegoapi.exceptions.RemoteServerException;
+import com.pokegoapi.util.SystemTimeImpl;
 import okhttp3.OkHttpClient;
 
 import java.util.Scanner;
@@ -11,26 +14,17 @@ import java.util.Scanner;
  */
 
 public class GoogleUserLogger {
+    private String username, password;
 
     public GoogleUserLogger() {
         OkHttpClient httpClient = new OkHttpClient();
 
         try {
             // Instantiate a provider, it will give an url
-            GoogleUserCredentialProvider provider =
-                    new GoogleUserCredentialProvider(httpClient);
-
-            // In this url, you will get a code for the google account that is logged
-            System.out.println("Please go to " + provider.LOGIN_URL);
-            System.out.print("Enter authorisation code: ");
-
-            // Ask the user to enter it in the standard input
-            Scanner sc = new Scanner(System.in);
-            String authCode = sc.nextLine();
-
-            // we should be able to login with this token
-            provider.login(authCode);
-            System.out.println("Refresh token:" + provider.getRefreshToken());
+            GoogleAutoCredentialProvider provider =
+                    new GoogleAutoCredentialProvider(httpClient, username, password, new SystemTimeImpl());
+            PokemonGo go = new PokemonGo(httpClient);
+            // go.login(provider);
         } catch (LoginFailedException
                 | RemoteServerException
                 | CaptchaActiveException e) {
