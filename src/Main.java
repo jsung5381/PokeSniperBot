32f7eba26c6d91dca7e89bfbcdfba910d0ea947b
jsung5381 <jsung5381@naver.com>
2017-02-10 23:03:47 +0900

    Implemented login and locate methods.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,38 @@
+import com.pokegoapi.api.PokemonGo;
+import com.pokegoapi.auth.GoogleAutoCredentialProvider;
+import com.pokegoapi.util.SystemTimeImpl;
+import com.pokegoapi.util.hash.HashProvider;
+import com.pokegoapi.util.hash.legacy.LegacyHashProvider;
+import okhttp3.OkHttpClient;
 
 /**
  * Created by jsh3571 on 10/02/2017.
  */
 public class Main {
+    private static PokemonGo api;
 
     public static void main(String[] args) {
+        login(args[0], args[1]);
+        locate(37.497942,127.0254323, 1);
 
+    }
+
+    private static void login(String username, String password) {
+        OkHttpClient http = new OkHttpClient();
+        api = new PokemonGo(http);
+        try {
+            GoogleAutoCredentialProvider provider =
+                    new GoogleAutoCredentialProvider(
+                            http, username, password, new SystemTimeImpl());
+            HashProvider hasher = new LegacyHashProvider();
+
+            api.login(provider, hasher);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void locate(double latitude, double longtitude, double altitude) {
+        api.setLocation(latitude, longtitude, altitude);
     }
 }
